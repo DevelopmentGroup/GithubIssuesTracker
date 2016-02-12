87d8edbdd2bd6d64618fa11a6854b3f91687ba43
Josh <jmc6yz@virginia.edu>
2016-02-12 06:46:28 -0500

    Formatting and Layout Finished

--- public/js/main.js
+++ public/js/main.js
@@ -12,9 +12,38 @@ jQuery.getIssues = function(username, repoName, pageNumber, callback) {
 //Displays up to 30 latest issues into the target div
 jQuery.fn.getAndDisplayIssues = function(username, repoName, pageNumber) {
     this.html("<span>Getting issues for " + username +"'s project " + repoName + ".</span>");
-     
+    $('#page-number').html("<span>page " + currentPage +"</span>");
+
+
+
+    //do right column info display
+    
+
+    $.ajax({
+    url: 'https://api.github.com/repos/' + username + '/' +repoName,
+    dataType: "json",
+    success: function(data){
+      $('#repo-name').html("<a href=\'" + data.html_url +"\'><h3>" + data.full_name + "</h3></a>");
+      $('#repo-image').html("<a href=\'" + data.owner.html_url +"\'><img width=\'70%\' height=\'70%\' src =\'" + data.owner.avatar_url +"\'></a>");
+      $('#open-issues').html("<h5>" + data.open_issues + " open issues</h5>");
+      $('#watched').html("<h5>watched by " + data.subscribers_count + " users</h5>");
+      $('#forked').html("<h5>forked by " + data.network_count + " users</h5>");
+      $('#starred').html("<h5>starred by " + data.stargazers_count + " users</h5>");
+    },
+    error: function(data){
+      //nothing
+    },
+    complete: function(data) {
+      //alert('complete')
+    }})
+
+
     var target = this;
-    $.getIssues(username, repoName, pageNumber, function(data) {
+
+    $.ajax({
+    url: 'https://api.github.com/repos/' + username + '/' +repoName + '/issues?page='+pageNumber,
+    dataType: "json",
+    success: function(data){
         var repos = data; // JSON Parsing
 
         //check if the project has no issues.
@@ -22,29 +51,60 @@ jQuery.fn.getAndDisplayIssues = function(username, repoName, pageNumber) {
         	alert("This project either has no issues (unlikely!) or we failed to retrieve them.");
         }
         sortByUpdateDate(repos);
-        console.log(repos);    
+        console.log(repos);
+
+
+
+           
      
         var list = $('<dl/>');
         target.empty().append(list);
         counterX = 0;
         $(repos).each(function() {
         	counterX++;
+        	var formattedBody = codeFormatText(this.body); 
             if (this.name != (username.toLowerCase()+'.github.com')) {
-            	var trimmedBody = trimStringLength(this.body);
-            	list.append('<div class=\'card card-1\'><div class=\'row\' style=\'margin:3em;\'><div style=\'\' class=\"col-sm-2\"><img width=75px height=75px src=' + this.user.avatar_url +'></div><div class=\"col-sm-10\"><div id=\'target' + counterX + '\'></div>' + '</div></br>'+'</div></div>');
+            	var trimmedBody = trimStringLength(formattedBody);
+            	list.append('<div class=\'card card-1\' style=\'margin:3em;\'><div class=\'row\' style=\'margin:0em;\'><div style=\'padding-left:0px;\' class=\"col-sm-3\"><a href=\''+this.user.html_url +'\'><img width=100% height=100% src=' + this.user.avatar_url +'></a></div><div class=\"col-sm-9\"><div id=\'target' + counterX + '\'></div>' + '</div></br>'+'</div></div>');
                 var targetDiv = "#target" + counterX;
                 //console.log(targetDiv);
-                $(targetDiv).append('<a href="'+ './issue.html?number=' + this.number + '&owner=' + username + '&repo=' + repoName +'">' +'#' + this.number + ": " + this.title + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em>&nbsp;&nbsp;&nbsp;&nbsp;');
+                $(targetDiv).append('<div style=\'font-size:1.3em\'><a href="'+ './issue.html?number=' + this.number + '&owner=' + username + '&repo=' + repoName +'">' +'#' + this.number + ": " + this.title + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></div>');
                 
-                for (var i = 0; i < this.labels.length; i++){
-                	$(targetDiv).append('<span style=color:' + this.labels[i].color+ '>label:' + this.labels[i].name + '</span>');
-                }
-                $(targetDiv).append('<br>' + trimmedBody + '');
+                if(this.labels.length > 1){
+                	$(targetDiv).append('<br>labels: ');
+	                for (var i = 0; i < this.labels.length; i++){
+	                	if((i+1)<this.labels.length){
+	                		$(targetDiv).append('<span style=\'color:#' + this.labels[i].color+ '\'>' + this.labels[i].name + ', </span>');
+	                	} else {
+	                		$(targetDiv).append('<span style=\'color:#' + this.labels[i].color+ '\'>' + this.labels[i].name + ' </span>');
+	                	}
+	                	
+	                }
+            	} else if (this.labels.length == 1){
+            		$(targetDiv).append('<br>label: ');
+            		for (var i = 0; i < this.labels.length; i++){
+            			$(targetDiv).append('<span style=\'color:#' + this.labels[i].color+ '\'>' + this.labels[i].name + ' </span>');
+            		}
+            	} else {
+            		//do nothing
+            	}
+                $(targetDiv).append('<br></br><div style=\'white-space:pre-wrap\'>' + trimmedBody + '</div>');
                 //console.log(this.user.avatar_url);
                 // list.append('</div></br>'+'</div>')
             }
         });      
-      });
+    },
+    error: function(data){
+      alert("Error: could not retrieve target repository.");
+    },
+    complete: function(data) {
+      //alert('complete')
+    }})
+
+
+
+
+    
     
     //sorts input repositories by date
     function sortByUpdateDate(repos) {
@@ -97,6 +157,9 @@ function submitTargetRepo(){
 	inputRepoName = $("#input-repo-name").val();
 	console.log(inputOwner);
 	$("#issues-list").getAndDisplayIssues(inputOwner, inputRepoName, 1);
+	currentPage = 1;
+	$('#page-buttons').css("display", "block");
+	//$('#page-buttons').css("text-align", "center");
 }
 
 //validates a Github username (from the input field "input-owner") and returns a boolean.
@@ -137,11 +200,16 @@ var currentPage = 1;
 function getNextIssues(){
 	$("#issues-list").getAndDisplayIssues(inputOwner, inputRepoName, currentPage + 1);
 	currentPage++;
+	$('#page-number').html("<span>page " + currentPage + "</span>");
 }
 
 function getPreviousIssues(){
 	$("#issues-list").getAndDisplayIssues(inputOwner, inputRepoName, currentPage - 1);
 	currentPage--;
+	if(currentPage <= 0){
+		currentPage = 1;
+	}
+	$('#page-number').html("<span>page " + currentPage + "</span>");
 }
 
 
@@ -180,15 +248,30 @@ jQuery.fn.getSingleIssueJQ = function(owner, repoName, issueNumber) {
       // }
       // target.empty().append(list);
       if (data.name != (owner.toLowerCase()+'.github.com')) {
-            	list.append('<div class=\'card card-2\' style=\'padding-top:1em\'><div class=\'row\' style=\'margin:3em;\'><div class=\"col-sm-4\"><img width=250px height=250px src=' + data.user.avatar_url +'></div><div class=\"col-sm-8\" style=\'white-space:pre-wrap;font-size:2.5em; padding-left:1em\'><div id=\'targetTitle\'></div></div><div class=\'row\'><div class=\'col-md-12\'><div id=\'targetDiv' + '\'></div></div></div></br>'+'</div></div>');
+            	list.append('<div class=\'card card-2\' style=\'padding-top:1em\'><div class=\'row\' style=\'margin:3em;\'><div class=\"col-sm-4\"><img width=250px height=250px src=' + data.user.avatar_url +'></div><div class=\"col-sm-8\" style=\'font-size:2.5em; padding-left:1em\'><div id=\'targetTitle\'></div></div><div class=\'row\'><div class=\'col-md-12\'><div id=\'targetDiv' + '\'></div></div></div></br>'+'</div></div>');
                 var targetDiv = "#targetDiv";
                 //console.log(targetDiv);
-                $('#targetTitle').append('<a href="'+ (data.homepage?data.homepage:data.html_url) +'">' +'#' + data.number + "<br>" + data.title + '</a> <em></br>' +data.state +(data.language?('('+data.language+')'):'')+'</em>&nbsp;');
+                $('#targetTitle').append('<a href="'+ (data.homepage?data.homepage:data.html_url) +'">' +'#' + data.number + "<br>" + data.title + '</a> <em></br>' +data.state +(data.language?('('+data.language+')'):'')+'</em>&nbsp;<br><br><br>');
                 
-                for (var i = 0; i < data.labels.length; i++){
-                	$(targetDiv).append('<span style=color:' + data.labels[i].color> + 'label:' + data.labels[i].name + '</span>');
-                }
-                $(targetDiv).append('<br><div style=\'white-space:pre-wrap\'>' + formattedBody + '</div>');
+                if(data.labels.length > 1){
+                	$(targetDiv).append('labels: ');
+	                for (var i = 0; i < data.labels.length; i++){
+	                	if((i+1)<data.labels.length){
+	                		$(targetDiv).append('<span style=\'color:#' + data.labels[i].color+ '\'>' + data.labels[i].name + ', </span>');
+	                	} else {
+	                		$(targetDiv).append('<span style=\'color:#' + data.labels[i].color+ '\'>' + data.labels[i].name + ' </span>');
+	                	}
+	                	
+	                }
+            	} else if (data.labels.length == 1){
+            		$(targetDiv).append('label: ');
+            		for (var i = 0; i < data.labels.length; i++){
+            			$(targetDiv).append('<span style=\'color:#' + data.labels[i].color+ '\'>' + data.labels[i].name + ' </span>');
+            		}
+            	} else {
+            		//do nothing
+            	}
+                $(targetDiv).append('<br><br><br><div style=\'white-space:pre-wrap\'>' + formattedBody + '</div>');
                 //console.log(this.user.avatar_url);
                 // list.append('</div></br>'+'</div>')
             }
@@ -203,7 +286,7 @@ jQuery.fn.getSingleIssueJQ = function(owner, repoName, issueNumber) {
 }
 
 
-//This function takes an input string and returns a string where ''' has been correctly replaced with the <pre><code> tag(s)
+//This function takes an input string and returns a string where ''' has been correctly replaced with the <pre><code> tag(s). Also checks for bold.
 function codeFormatText(input){
 	//var p = '%22';
 	var first = true;
@@ -219,6 +302,20 @@ function codeFormatText(input){
 			first = true;
 		}
 	}
+
+	var first1 = true;
+	while (input.indexOf("**") != -1){
+		if(first1 == true){
+			input = input.replace("**", "<strong>");
+			//console.log(true);
+			first1 = false;
+		} else {
+			input = input.replace("**", "</strong>");
+			first1 = true;
+		}
+	}
+
+
 	//var x = input.indexOf("```");
 	//console.log("HERE: " + x);
 	//console.log(input);
